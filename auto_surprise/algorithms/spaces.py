edb7bf7b6c4c77281ea75383ab9a655458d2a508
Rohan Anand <thededlier@gmail.com>
2020-04-14 11:20:55 +0100

    increase overall ranges

--- auto_surprise/algorithms/spaces.py
+++ auto_surprise/algorithms/spaces.py
@@ -6,21 +6,21 @@ from hyperopt import hp
 
 SVD_DEFAULT_SPACE = {
     'n_factors': hp.choice('n_factors', range(1, 100)),
-    'n_epochs': hp.choice('n_epochs', range(5, 100)),
-    'lr_bu': hp.uniform('lr_bu', 0.0001, 0.3),
-    'lr_bi': hp.uniform('lr_bi', 0.0001, 0.3),
-    'lr_pu': hp.uniform('lr_pu', 0.0001, 0.3),
-    'lr_qi': hp.uniform('lr_qi', 0.0001, 0.3),
-    'reg_bu': hp.uniform('reg_bu', 0.0001, 0.3),
-    'reg_bi': hp.uniform('reg_bi', 0.0001, 0.3),
-    'reg_pu': hp.uniform('reg_pu', 0.0001, 0.3),
-    'reg_qi': hp.uniform('reg_qi', 0.0001, 0.3)
+    'n_epochs': hp.choice('n_epochs', range(5, 200)),
+    'lr_bu': hp.uniform('lr_bu', 0.0001, 0.5),
+    'lr_bi': hp.uniform('lr_bi', 0.0001, 0.5),
+    'lr_pu': hp.uniform('lr_pu', 0.0001, 0.5),
+    'lr_qi': hp.uniform('lr_qi', 0.0001, 0.5),
+    'reg_bu': hp.uniform('reg_bu', 0.0001, 0.5),
+    'reg_bi': hp.uniform('reg_bi', 0.0001, 0.5),
+    'reg_pu': hp.uniform('reg_pu', 0.0001, 0.5),
+    'reg_qi': hp.uniform('reg_qi', 0.0001, 0.5)
 }
 
 SVDPP_SPACE = {
     **SVD_DEFAULT_SPACE,
-    'lr_yj': hp.uniform('lr_yj', 0.0001, 0.3),
-    'reg_yj': hp.uniform('reg_yj', 0.0001, 0.3)
+    'lr_yj': hp.uniform('lr_yj', 0.0001, 0.5),
+    'reg_yj': hp.uniform('reg_yj', 0.0001, 0.5)
 }
 
 SIMILARITY_OPTIONS_SPACE = {
@@ -30,25 +30,25 @@ SIMILARITY_OPTIONS_SPACE = {
 }
 
 KNN_DEFAULT_SPACE = {
-    'k': hp.choice('k', range(1, 100)),
+    'k': hp.choice('k', range(1, 500)),
     'min_k': hp.choice('min_k', range(1, 10)),
     'sim_options': SIMILARITY_OPTIONS_SPACE
 }
 
 NMF_DEFAULT_SPACE = {
     'n_factors': hp.choice('n_factors', range(1, 100)),
-    'n_epochs': hp.choice('n_epochs', range(5, 100)),
-    'lr_bu': hp.uniform('lr_bu', 0.0001, 0.3),
-    'lr_bi': hp.uniform('lr_bi', 0.0001, 0.3),
-    'reg_bu': hp.uniform('reg_bu', 0.0001, 0.3),
-    'reg_bi': hp.uniform('reg_bi', 0.0001, 0.3),
-    'reg_pu': hp.uniform('reg_pu', 0.0001, 0.3),
-    'reg_qi': hp.uniform('reg_qi', 0.0001, 0.3),
+    'n_epochs': hp.choice('n_epochs', range(5, 200)),
+    'lr_bu': hp.uniform('lr_bu', 0.0001, 0.5),
+    'lr_bi': hp.uniform('lr_bi', 0.0001, 0.5),
+    'reg_bu': hp.uniform('reg_bu', 0.0001, 0.5),
+    'reg_bi': hp.uniform('reg_bi', 0.0001, 0.5),
+    'reg_pu': hp.uniform('reg_pu', 0.0001, 0.5),
+    'reg_qi': hp.uniform('reg_qi', 0.0001, 0.5),
     'biased': hp.choice('biased', [False, True])
 }
 
 CO_CLUSTERING_DEFAULT_SPACE = {
-    'n_cltr_u': hp.choice('n_cltr_u', range(1, 30)),
-    'n_cltr_i': hp.choice('n_cltr_i', range(1, 30)),
-    'n_epochs': hp.choice('n_epochs', range(5, 100)),
+    'n_cltr_u': hp.choice('n_cltr_u', range(1, 100)),
+    'n_cltr_i': hp.choice('n_cltr_i', range(1, 100)),
+    'n_epochs': hp.choice('n_epochs', range(5, 200)),
 }
