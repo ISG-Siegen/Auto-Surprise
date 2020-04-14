3dc6fa1b93ae60adb84af605a6f4f13f4eea0a1e
Rohan Anand <thededlier@gmail.com>
2020-04-14 11:53:24 +0100

    use baseline option for KNNBaseline

--- auto_surprise/algorithms/spaces.py
+++ auto_surprise/algorithms/spaces.py
@@ -26,13 +26,33 @@ SVDPP_SPACE = {
 SIMILARITY_OPTIONS_SPACE = {
     'name': hp.choice('name', ['cosine', 'msd', 'pearson', 'pearson_baseline']),
     'user_based': hp.choice('user_based', [False, True]),
+    'min_support': hp.choice('min_support', range(1, 100)),
     'shrinkage': hp.choice('shrinkage', range(1, 300))
 }
+
+BSL_OPTIONS_SPACE = hp.choice('bsl_options', [
+    {
+        'method': 'als',
+        'reg_i': hp.choice('reg_i', range(1, 100)),
+        'reg_u': hp.choice('reg_u', range(1, 100)),
+        'n_epochs': hp.choice('n_epochs', range(5, 200)),
+    },
+    {
+        'method': 'sgd',
+        'reg': hp.uniform('reg', 0.0001, 0.5),
+        'learning_rate': hp.uniform('learning_rate', 0.0001, 0.5)
+    }
+])
 
 KNN_DEFAULT_SPACE = {
     'k': hp.choice('k', range(1, 500)),
     'min_k': hp.choice('min_k', range(1, 10)),
     'sim_options': SIMILARITY_OPTIONS_SPACE
+}
+
+KNN_BASELINE_SPACE = {
+    **KNN_DEFAULT_SPACE,
+    'bsl_options': BSL_OPTIONS_SPACE
 }
 
 NMF_DEFAULT_SPACE = {
