803073866b4f2f14f5203813ce7db96989ad1af1
Rohan Anand <thededlier@gmail.com>
2020-06-15 18:41:00 +0530

    bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="auto-surprise",
-    version="0.1.1",
+    version="0.1.2",
     author="Rohan Anand",
     author_email="anandr@tcd.ie",
     description="A python package that automates algorithm selection and hyperparameter tuning for the recommender system library Surprise",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BeelGroup/Auto-Surprise",
-    download_url="https://github.com/BeelGroup/Auto-Surprise/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/BeelGroup/Auto-Surprise/archive/v0.1.2.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
